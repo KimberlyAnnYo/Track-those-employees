23b399722ad5f808ba9078ce9ae81cc759dd60d9
Kimberly West <kdenton993@gmail.com>
2022-07-10 19:00:49 -0600

    building database

--- app.js
+++ app.js
@@ -2,4 +2,36 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const ctable = require("consol.table");
 
+class Database {
+    constructor(config) {
+        this.connection = mysql.createConnection(config);
+    }
+
+    query(sql, args) {
+        return new Promise((resolve, reject) => {
+            this.connection.query(sql, args, (err, rows) => {
+                if(err)
+                return reject(err);
+                resolve(rows);
+            });
+        });
+    }
+    close() {
+        return new Promise((resolve, reject) => {
+            this.connection.end(err => {
+                if(err)
+                return reject(err);
+                resolve();
+            });
+        });
+    }
+}
+
+const db = new Database({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Gatewat3!"
+    database:"employeeDB"
+});
 
