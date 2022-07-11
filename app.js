59efdb241053fc2159369d31153aebca2cb01009
Kimberly West <kdenton993@gmail.com>
2022-07-10 20:30:09 -0600

    trying to fix database crashing at the connection

--- app.js
+++ app.js
@@ -1,43 +1,20 @@
 const mysql = require("mysql2");
-const inquirer = require("inquirer");
+import "inquirer";
+// const inquirer = require("inquirer");
 const ctable = require("console.table");
 
-// class Database {
-//     constructor(config) {
-//         this.connection = mysql.createConnection(config);
-//     }
 
-//     query(sql, args) {
-//         return new Promise((resolve, reject) => {
-//             this.connection.query(sql, args, (err, rows) => {
-//                 if(err)
-//                 return reject(err);
-//                 resolve(rows);
-//             });
-//         });
-//     }
-//     close() {
-//         return new Promise((resolve, reject) => {
-//             this.connection.end(err => {
-//                 if(err)
-//                 return reject(err);
-//                 resolve();
-//             });
-//         });
-//     }
-// }
+const connection = mysql.createConnection({
+    host: 'localhost',
+    password: "Gateway3!",
+    user: 'root',
+    database: 'employee'
+  });
 
-// const db = new Database({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "Gatewat3!",
-//     database:"employeeDB"
-// });
 
 // async function showEmployeeSummary() {
 //     console.log(' ');
-//     await db.query('SELECT e.id, e.first_name AS First_Name, e.last_name AS Last_Name, title AS Title, salary AS Salary, name AS Department, CONCAT(m.first_name, " ", m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id', (err, res) => {
+//     await connection.query('SELECT e.id, e.first_name AS First_Name, e.last_name AS Last_Name, title AS Title, salary AS Salary, name AS Department, CONCAT(m.first_name, " ", m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id', (err, res) => {
 //         if (err) throw err;
 //         console.table(res);
 //         runApp();
