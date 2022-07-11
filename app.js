ba4a832a074e2345a0713ee6b79d2251d250076a
Kimberly West <kdenton993@gmail.com>
2022-07-10 19:17:24 -0600

    adding new employees

--- app.js
+++ app.js
@@ -96,19 +96,6 @@ async function addEmployee() {
             message: "Choose employee role:",
             choices: positions.map(obj => obj.title)
         },
-        {
-            name: "manager",
-            type: "list",
-            message: "Choose the employee's manager:",
-            choices: managers.map(obj => obj.Manager)
-        }
-    ]).then(answers => {
-        let positionDetails = positions.find(obj => obj.title === answers.role);
-        let manager = managers.find(obj => obj.Manager === answers.manager);
-        db.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?)", [[answers.firstName.trim(), answers.lastName.trim(), positionDetails.id, manager.id]]);
-        console.log("\x1b[32m", `${answers.firstName} was added to the employee database!`);
-        runApp();
-    });
-};
+
 
 
